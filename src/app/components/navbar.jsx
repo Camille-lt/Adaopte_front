ee121fced96ced49f02e710b3a59a453d2884c6c
Vanessa Valencia-Garcia <vanessa_vg@outlook.fr>
2025-07-08 16:26:46 +0200

    menu hamburger tbc

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -2,11 +2,28 @@
 "use client"
 
 import Link from 'next/link';
+import { useEffect, useState} from 'react';
+import { createIcons, icons } from 'lucide';
+import styles from '../navbar.module.css';
 
 export default function Navbar() {
+  useEffect(() => {
+    // Crée les icônes après le montage du composant
+    createIcons({ icons });
+  }, []);
+
+  // Pour que la navbar se déplie et se replie
+        // const menuHamburger = document.querySelector(".menu-hamburger")
+        // const navLinks = document.querySelector(".nav-links")
+ 
+        // menuHamburger.addEventListener('click',()=>{
+        // navLinks.classList.toggle('mobile-menu')
+        // })
+
     return (
-       <nav style={{ padding: '1rem', backgroundColor: '#222', color: '#fff' }}>
-  <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none', margin: 0 }}>
+       <nav className={styles.navbar}>
+        <div className={styles.navbarLinks}>
+  <ul className={styles.navbarLinks}>
     <li>
       <Link href="/" style={{ color: 'white', textDecoration: 'none' }}>
         Accueil
@@ -33,6 +50,8 @@ export default function Navbar() {
       </Link>
     </li>
   </ul>
+  </div>
+  <i className='menu' data-lucide="menu"></i>
 </nav>
     )
 }
