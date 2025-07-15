46ffd81b2de00664d0e4b9fc9c0eb5311f213a82
Camille-lt <camillelebigot5@gmail.com>
2025-07-15 22:02:22 +0200

    add new style of button

--- src/app/components/button.jsx
+++ src/app/components/button.jsx
@@ -1,20 +1,23 @@
-'use client'; // Important pour que ce soit un composant client (interactif)
+'use client';
 
-export default function Button({ onClick, children }) {
-  return (
-    <button
-      type="button"
-      onClick={onClick}
-      style={{
-        padding: '10px 20px',
-        backgroundColor: '#416D6C',
-        color: 'white',
-        border: 'none',
-        borderRadius: '56px',
-        cursor: 'pointer',
-      }}
-    >
-      {children}
-    </button>
-  );
+
+export default function Button({ onClick, children, style = {} }) {
+ return (
+   <button
+     type="button"
+     onClick={onClick}
+     style={{
+       padding: '10px 20px',
+       backgroundColor: '#416D6C',
+       color: 'white',
+       border: 'none',
+       borderRadius: '56px',
+       cursor: 'pointer',
+       marginTop: '1.5rem',
+       ...style, // Permet de changer le style de certains buttons
+     }}
+   >
+     {children}
+   </button>
+ );
 }
