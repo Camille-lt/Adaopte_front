1ec217099de5f979152af5051e01b9c8288955f1
Camille-lt <camillelebigot5@gmail.com>
2026-01-17 20:32:24 +0100

    updated image for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Ceci autorise toutes les images en HTTPS
+      },
+    ],
+  },
+};
 
 export default nextConfig;
